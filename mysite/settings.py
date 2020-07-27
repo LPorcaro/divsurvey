6faa95aaf76fa869026f84953207fe5a406e177b
LPorcaro <lorenzo.porcaro@gmail.com>
2020-07-27 16:02:59 +0200

    Fix typo

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'r2t5*wsl-a=lzzzy$er&5y1-85ta=#&$4)29fm6!-=q*c$rbfh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
